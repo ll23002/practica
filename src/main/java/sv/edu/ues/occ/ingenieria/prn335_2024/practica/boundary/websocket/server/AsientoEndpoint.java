44d9bc8c4776165041515a04b753497078fa5440
milagro <zv23001@ues.edu.sv>
2024-12-02 19:58:09 -0600

    websockets faltantes

---
+++ src/main/java/sv/edu/ues/occ/ingenieria/prn335_2024/practica/boundary/websocket/server/AsientoEndpoint.java
@@ -0,0 +1,47 @@
+package sv.edu.ues.occ.ingenieria.prn335_2024.practica.boundary.websocket.server;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.websocket.OnClose;
+import jakarta.websocket.OnMessage;
+import jakarta.websocket.OnOpen;
+import jakarta.websocket.Session;
+import jakarta.websocket.server.ServerEndpoint;
+
+import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@ServerEndpoint("/notificadorasiento")
+@ApplicationScoped
+@Named
+
+public class AsientoEndpoint implements Serializable {
+    @Inject
+    ManejadorSesionesWS manejadorSesiones;
+
+    @OnOpen
+    public void abrir(Session sesion) {
+        System.out.println("Abriendo sesion");
+        manejadorSesiones.agregarSesion(sesion);
+    }
+
+    @OnMessage
+    public void propagarMensaje(Session sesion, String mensaje) {
+        for (Session se : this.manejadorSesiones.getSESIONES()) {
+            if (se != null && se.isOpen()) {
+                try {
+                    se.getBasicRemote().sendText(mensaje);
+                } catch (Exception e) {
+                    Logger.getLogger(getClass().getName()).log(Level.SEVERE, e.getMessage(), e);
+                }
+            }
+        }
+    }
+
+    @OnClose
+    public void cerrar(Session sesion) {
+        manejadorSesiones.eliminarSesion(sesion);
+    }
+}
